f91bbb217d3856314c587e79a13c90d9b89cf838
Valdio Veliu <valdioveliu@gmail.com>
2019-05-20 21:24:52 +0200

    adding docs

--- README.md
+++ README.md
@@ -1,8 +1,89 @@
 # react-native-js-injector
-React Native package for executing injected JS on WebView components. (cookies enabled)
+React Native WebView with "super powers". This package is basically a WebView with some extra perks included.
+
+1. Possibility to inject JS scripts easily.
+2. Browser cookies enabled by default. 
+3. Auto Height `WebView` options, in order to use `WebView`s inside components like `ScrollView`
+
+**Caution!** This package can only be used with row HTML content. If a `uri` is passed as the `source` prop this component will return an empty `<View/>`.
+
+### Install
+
+```
+npm install react-native-js-injector --save
+```
+    
+```
+yarn add react-native-js-injector
+```
+
+This library is depended of the latest updates of the `WebView` component. 
+At the current time of this release, the react team suggests using the [RN community backed WebView](https://github.com/react-native-community/react-native-webview) as they are removing `WebView` from the react core.
+
+Install the following WebView
+```
+yarn add react-native-webview
+
+react-native link react-native-webview
+```
+
+If there are any issues check out the [Getting-Started guide](https://github.com/react-native-community/react-native-webview/blob/master/docs/Getting-Started.md).
 
 
 
-## Debugging
+### Usage
+
+```typescript jsx
+import {WebViewInjector, INJECTOR_TYPE} from 'react-native-js-injector'
+const htmlTemplate = `<p>Hello!</p`
+//example script
+const script = `<script>document.body.appendChild(document.createElement("hr"));</script>`
+export default class App extends Component {
+  render() {
+    return (<View style={styles.container}>
+          <WebViewInjector
+            injectScript={script}
+            scrollEnabled={false}
+            // style={{backgroundColor: 'red'}}
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+            type={INJECTOR_TYPE.AUTO_HEIGHT}
+            source={{html: htmlTemplate}}
+          />
+      </View>
+    )
+  }
+}
+```
+
+
+## Props
+The props are build to follow the same guidelines as the standard props that `WebView` accepts.
+
+- **`source`** _(Object)_ - JS object containing the raw HTML code. Example: `{html: <div></div>}`
+- **`type`** _(String)_ - A flag specifying if the component should use the auto-height perks or not. INJECTOR_TYPE.DEFAULT || INJECTOR_TYPE.AUTO_HEIGHT. **Not required!**
+- **`injectScript`** _(String)_ - String containing the script that will be executed on the `WebView`. Read the guideline scripts bellow.
+- **`*****`** _(PROPS)_ - The component is build to accept any of the default props that are supported by the standard `WebView`.
+
+### scripts guidelines
+In order to execute scripts make sure they are [`**IIFE** (Immediately Invoked Function Expression)`](https://developer.mozilla.org/en-US/docs/Glossary/IIFE).
+Make sure the `injectScript` props is passed as `String`. You can use one of the following examples to pass as the value of `injectScript` prop. 
+
+```typescript jsx
+const script1 = (function () {
+                    //statements...
+                })();
+// usage
+injectScript={script1.toString()}
+
+
+
+const script2 = `<script>console.log('worthless script');</script>`
+// usage
+injectScript={script2}
+``` 
+
+
+### Debugging
 In order to remotely debug WebView issues read the following steps.
 [Debugging Info](https://github.com/valdio/react-native-js-injector/blob/master/Debugging.md)
--- example/App.js
+++ example/App.js
@@ -3,16 +3,33 @@ import {StyleSheet, Text, ScrollView, View} from 'react-native'
 import {WebViewInjector, INJECTOR_TYPE} from 'react-native-js-injector'
 
 const htmlTemplate = `
-<p>&ldquo;<em>Dueli i unifikimit t&euml; titujve kampion&euml; bote n&euml; pesh&euml;n e r&euml;nd&euml; duhet organizuar me &ccedil;do kusht. Madje ajo duhet t&euml; jet&euml; sfida e radh&euml;s mes tyre</em>&rdquo;, u shpreh Eddie Hearn, menaxheri i boksierit britanik Anthony Joshua. Ky i fundit &euml;sht&euml; kampion bote n&euml; kategorit&euml; IBF, WBA dhe WBO.</p><p>Kjo deklarat&euml; erdhi sot, pas fitores q&euml; kampioni tjet&euml;r i bot&euml;s n&euml; pesh&euml;n e r&euml;nd&euml;, Deontay Wilder (kategoria WBC), arriti ndaj Dominic Breazeale nj&euml; dit&euml; m&euml; par&euml;. &ldquo;<em>Nuk ka m&euml; justifikime p&euml;r t&euml; shmangur k&euml;t&euml; sfid&euml;, nga t&euml; dy boksier&euml;t</em>&rdquo;, tha Eddie Hearn.</p><p>&ldquo;<em>Fitorja e Wilder ishte mahnit&euml;se. Ai e b&euml;ri detyr&euml;n e tij, tani i takon Joshua t&euml; b&euml;j&euml; detyr&euml;n e tij, p&euml;r t&euml; fituar duelin ndaj Ruiz Jr. Gjithsesi, mendoj se edhe ai do t&euml; triumfoj&euml; dhe do t&euml; kemi nj&euml; p&euml;rballje t&euml; madhe mes tyre</em>&rdquo;, tha Eddie Hearn.</p><p>&ldquo;<em>Dueli mes Joshua dhe Wilder do t&euml; ishte p&euml;rballja m&euml; e madhe e peshave t&euml; r&euml;nda n&euml; dekad&euml;n e fundit. N&euml;se k&euml;t&euml; her&euml; i nisim bisedimet me k&euml;mb&euml;n e mbar&euml;, mund ta realizojm&euml; shum&euml; shpejt. Shpresoj q&euml; dy boksier&euml;t ta kuptojn&euml; k&euml;t&euml; gj&euml;, pasi ka r&euml;nd&euml;si p&euml;r ta, sportin, fansat. &Euml;sht&euml; &ldquo;qershia mbi tort&euml;&rdquo; n&euml; pesh&euml;n e r&euml;nd&euml;</em>&rdquo;, tha Hearn.</p>
-<p>&ldquo;<em>Dueli i unifikimit t&euml; titujve kampion&euml; bote n&euml; pesh&euml;n e r&euml;nd&euml; duhet organizuar me &ccedil;do kusht. Madje ajo duhet t&euml; jet&euml; sfida e radh&euml;s mes tyre</em>&rdquo;, u shpreh Eddie Hearn, menaxheri i boksierit britanik Anthony Joshua. Ky i fundit &euml;sht&euml; kampion bote n&euml; kategorit&euml; IBF, WBA dhe WBO.</p><p>Kjo deklarat&euml; erdhi sot, pas fitores q&euml; kampioni tjet&euml;r i bot&euml;s n&euml; pesh&euml;n e r&euml;nd&euml;, Deontay Wilder (kategoria WBC), arriti ndaj Dominic Breazeale nj&euml; dit&euml; m&euml; par&euml;. &ldquo;<em>Nuk ka m&euml; justifikime p&euml;r t&euml; shmangur k&euml;t&euml; sfid&euml;, nga t&euml; dy boksier&euml;t</em>&rdquo;, tha Eddie Hearn.</p><p>&ldquo;<em>Fitorja e Wilder ishte mahnit&euml;se. Ai e b&euml;ri detyr&euml;n e tij, tani i takon Joshua t&euml; b&euml;j&euml; detyr&euml;n e tij, p&euml;r t&euml; fituar duelin ndaj Ruiz Jr. Gjithsesi, mendoj se edhe ai do t&euml; triumfoj&euml; dhe do t&euml; kemi nj&euml; p&euml;rballje t&euml; madhe mes tyre</em>&rdquo;, tha Eddie Hearn.</p><p>&ldquo;<em>Dueli mes Joshua dhe Wilder do t&euml; ishte p&euml;rballja m&euml; e madhe e peshave t&euml; r&euml;nda n&euml; dekad&euml;n e fundit. N&euml;se k&euml;t&euml; her&euml; i nisim bisedimet me k&euml;mb&euml;n e mbar&euml;, mund ta realizojm&euml; shum&euml; shpejt. Shpresoj q&euml; dy boksier&euml;t ta kuptojn&euml; k&euml;t&euml; gj&euml;, pasi ka r&euml;nd&euml;si p&euml;r ta, sportin, fansat. &Euml;sht&euml; &ldquo;qershia mbi tort&euml;&rdquo; n&euml; pesh&euml;n e r&euml;nd&euml;</em>&rdquo;, tha Hearn.</p>
+<div class="fr-embedly " data-original-embed="<a href='https://youtu.be/t-645fMmuD0' data-card-branding='0' class='embedly-card'></a>" style="height: 370px;">
+\t<a href="https://youtu.be/t-645fMmuD0" data-card-branding="0" class="embedly-card"></a>
+</div>
 `
+
+const script = `<script>
+  (function(w, d){
+   var id='embedly-platform', n = 'script';
+   if (!d.getElementById(id)){
+     w.embedly = w.embedly || function() {(w.embedly.q = w.embedly.q || []).push(arguments);};
+     var e = d.createElement(n); e.id = id; e.async=1;
+     e.src = ('https:' === document.location.protocol ? 'https' : 'http') + '://cdn.embedly.com/widgets/platform.js';
+     var s = d.getElementsByTagName(n)[0];
+     s.parentNode.insertBefore(e, s);
+   }
+  })(window, document);
+</script>`
+
+
 export default class App extends Component {
   render() {
     return (<View style={styles.container}>
         <ScrollView>
           <WebViewInjector
+            injectScript={script}
             scrollEnabled={false}
-            style={{backgroundColor: 'red'}}
+            // style={{backgroundColor: 'red'}}
             showsVerticalScrollIndicator={false}
             showsHorizontalScrollIndicator={false}
             type={INJECTOR_TYPE.AUTO_HEIGHT}
